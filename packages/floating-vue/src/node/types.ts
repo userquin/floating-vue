edb07a47dbc39c7c6bc826952215a45f3caad776
userquin <userquin@gmail.com>
2025-05-11 20:02:50 +0200

    chore: fix types

--- packages/floating-vue/src/node/types.ts
+++ packages/floating-vue/src/node/types.ts
@@ -2,12 +2,12 @@ export type DirectiveName = keyof typeof import('floating-vue/directives')
 
 export interface FloatingVueDirectivesOptions {
   /**
-     * Prefix FloatingVue directives (to allow use other directives with the same name):
-     * - when prefix set to `true` will use `Vuetify` => `v-floating-vue-<directive>: `v-floating-vue-tooltip`.
-     */
+   * Prefix FloatingVue directives (to allow users to use other directives with the same name):
+   * - when prefix set to `true` will use `FloatingVue` => `v-floating-vue-<directive>: `v-floating-vue-tooltip`.
+   */
   prefix?: true
   /**
-     * Directives to exclude.
-     */
+   * Directives to exclude.
+   */
   exclude?: DirectiveName[]
 }
